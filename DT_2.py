2607d43c10c8380ed9ff925381c4197cb15aaf64
Weiliang Xie <weiliangxie@hotmail.com>
2020-10-10 20:20:31 -0400

    change variable names

--- DT_2.py
+++ DT_2.py
@@ -7,27 +7,27 @@ from utility import *
 
 # Training set
 data_train2 = csvToList("train_2.csv")
-X_train1, y_train1 = getFeaturesAndClass(data_train2)
+X_train2, y_train2 = getFeaturesAndClass(data_train2)
 
 # Validation set
 data_val2 = csvToList("val_2.csv")
-X_val1, y_val1 = getFeaturesAndClass(data_val2)
+X_val2, y_val2 = getFeaturesAndClass(data_val2)
 
 # Base DT
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X_train1, y_train1)
-y_val_pred1 = clf.predict(X_val1)
+clf = clf.fit(X_train2, y_train2)
+y_val_pred2 = clf.predict(X_val2)
 
-cm = confusion_matrix(y_val_pred1, y_val1)
+cm = confusion_matrix(y_val_pred2, y_val2)
 # print(cm)
 
 # Test set
 data_test2 = csvToList("test_with_label_2.csv")
-X_test1, y_test1 = getFeaturesAndClass(data_test2)
+X_test2, y_test2 = getFeaturesAndClass(data_test2)
 
-y_test_pred1 = clf.predict(X_test1)
+y_test_pred2 = clf.predict(X_test2)
 
-cm = confusion_matrix(y_test_pred1, y_test1)
+cm = confusion_matrix(y_test_pred2, y_test2)
 print(cm)
 
 # Metrics
