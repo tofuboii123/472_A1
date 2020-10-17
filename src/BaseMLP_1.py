62a8f6bcc1c51366b0669253f88cf35bd4e2de46
tppham94 <43486951+tppham94@users.noreply.github.com>
2020-10-17 02:21:35 -0400

    So far probably missing some stuff

---
+++ src/BaseMLP_1.py
@@ -0,0 +1,36 @@
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+from utility import *
+
+# Getting the Training Data Set 
+data_train1 = csvToList("train_1.csv")
+X_train1, y_train1 = getFeaturesAndClass(data_train1)
+
+# Getting the Validation Set 
+data_val1 = csvToList("val_1.csv")
+X_val1, y_val1 = getFeaturesAndClass(data_val1)
+
+# Setting the number of iteration
+clf = MLPClassifier(max_iter=300)
+clf.fit(X_train1, y_train1)
+
+# Validation Set 
+data_test1 = csvToList("test_with_label_1.csv")
+
+# Separate features from classes
+X_test1, y_test1 = getFeaturesAndClass(data_test1)
+
+# Prediction 
+y_test1_pred1 = clf.predict(X_test1)
+
+cm = confusion_matrix(y_test1_pred1, y_test1)
+print(cm)
+
+# Metrics
+getMetrics(y_test1, y_test1_pred1)
+
+print(clf.score(X_test1, y_test1))
